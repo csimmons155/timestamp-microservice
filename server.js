e71a00c21fa8614d39110e88fd2945a2f1ec3714
Chris <csimmons155@gmail.com>
2017-01-22 21:08:37 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+//in c9 always use port: 8080
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
